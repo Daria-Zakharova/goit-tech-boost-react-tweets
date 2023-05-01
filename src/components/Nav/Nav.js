0339fb51dfc85b6b4b8cd5c518c73e7a8c006758
Daria-Zakharova <dariazakharova.smthelse@gmail.com>
2023-05-01 21:30:30 +0300

    fix links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,12 +7,12 @@ export const Nav = () => {
     <NavWrap>
       <ul>
         <li>
-          <NavLink to="/goit-test-react-tweets/" end>
+          <NavLink to="/goit-tech-boost-react-tweets/" end>
             <img src={logo} alt="logo" style={{ maxHeight: "30px" }} />
           </NavLink>
         </li>
         <li>
-          <NavLink to="/goit-test-react-tweets/tweets">Tweets</NavLink>
+          <NavLink to="/goit-tech-boost-react-tweets/tweets">Tweets</NavLink>
         </li>
       </ul>
     </NavWrap>
